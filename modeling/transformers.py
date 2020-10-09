6b058f637cae31fdb2ad1b91a1a46bb550fac42a
angel-hermon <angel.hermon.mail@gmail.com>
2020-10-09 23:07:44 +0300

    implement set_future_value, column selector and category encoder

--- modeling/transformers.py
+++ modeling/transformers.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+
+from pandas import DataFrame
+from typing import List
 from sklearn.preprocessing import FunctionTransformer 
 
 Volatility_group_labels = ['Low', 'Medium', 'High']
@@ -69,6 +72,50 @@ def calculate_days_close_above_30_moving_avarage(dataframe):
     dataframe['Days_close_above_30_moving_avarage'] = is_close_above_30_moving_avarage_cumsum.sub(is_close_above_30_moving_avarage_cumsum.mask(dataframe['Is_close_above_30_moving_avarage'] != 0).ffill(), fill_value=0).astype(int)
     return dataframe
 
+
+def generate_set_future_day_value(**kwargs):
+    r"""Generate a set future day value function
+    :param \**kwargs:
+        See below
+
+    :Keyword Arguments:
+        * *columns* (``str``) --
+          Column to set future value for
+        * *future_distance* (``int``) --
+          Max future days to set value for
+        * *step* (``int``) --
+          Set future step size
+
+    """
+    if not 'column' in kwargs:
+        raise Exception('generate_set_future_day_value expects a column name!')
+    if not 'future_distance' in kwargs:
+        raise Exception('generate_set_future_day_value expects a future_distance value!') 
+    
+    column = kwargs['column']
+    max_future_distance = kwargs['future_distance']
+    step = kwargs['step'] if 'step' in kwargs else -1    
+    
+    def inner_func(dataframe):
+        dataframe = dataframe.copy()
+        if step > 0:
+            for future_distance in range(1, max_future_distance, step):
+                for future_cell_index in range(future_distance, len(dataframe)):
+                    dataframe.loc[future_cell_index - future_distance,f'{column}_in_{future_distance}_days'] = dataframe.loc[future_cell_index, column]
+        else:
+            future_distance = max_future_distance
+            for future_cell_index in range(future_distance, len(dataframe)):
+                    dataframe.loc[future_cell_index - future_distance,f'{column}_in_{future_distance}_days'] = dataframe.loc[future_cell_index, column]
+        return dataframe
+    return inner_func
+
+def generate_column_excluder_func(columns: List[str]):
+    def inner_func(dataframe):
+        dataframe = dataframe.copy()
+        dataframe.drop(columns=columns, inplace=True, axis=1)
+        return dataframe
+    return inner_func
+
 def get_transformer_func_list():
     return [(func_tuple[0], FunctionTransformer(func_tuple[1])) for func_tuple in [ \
             ('calculate_volatility', calculate_voletility), \
@@ -76,9 +123,12 @@ def get_transformer_func_list():
             ('convert_date', convert_date), \
             ('sort_by_date', sort_by_date), \
             ('extract_day_of_week', extract_day_of_week), \
-            ('set_volatility_group_label', set_volatility_group_label), \
+            ('set_volatility_group_label', set_volatility_group_label),
+            ('set_30_day_future_close', generate_set_future_day_value(column='Close', future_distance=30, step=5)), \
             ('calculate_close_30_moving_avarage', calculate_close_30_moving_avarage), \
             ('set_is_close_below_30_moving_avarage_diff', set_is_close_below_30_moving_avarage_diff), \
             ('set_is_close_above_30_moving_avarage_diff', set_is_close_above_30_moving_avarage_diff), \
             ('calculate_days_close_below_30_moving_avarage', calculate_days_close_below_30_moving_avarage), \
-            ('calculate_days_close_above_30_moving_avarage', calculate_days_close_above_30_moving_avarage)]]
+            ('calculate_days_close_above_30_moving_avarage', calculate_days_close_above_30_moving_avarage), \
+            ('exclude_non_numeric_columns', generate_column_excluder_func(['Date']))
+          ]]
